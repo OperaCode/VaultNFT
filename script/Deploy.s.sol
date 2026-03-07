fa02520a5d8ff15575894d49ce338a3607626fd3
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2026-03-07 19:20:36 +0100

    vaultFile

---
+++ script/Deploy.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {Vault} from "../src/Vault.sol";
+import {VaultFactory} from "../src/VaultFactory.sol";
+import {VaultNFT} from "../src/VaultNFT.sol";
+
+contract DeployScript is Script{
+    
+}
